39a0033a34c1962061f0429cadb1e9803a3e7d56
Kadiri Mohan Kumar <136305228+kadirikumar-uptycs@users.noreply.github.com>
2025-02-06 22:33:26 +0530

    Added Script for Removing Tags from Host

---
+++ Remove Tags/README.md
@@ -0,0 +1,113 @@
+# Remove Host Tags Script
+
+This script is designed to remove tags from hosts using a CSV file and credentials provided by the user. The script reads the credentials and CSV file, retrieves the tags for each host, and removes the specified tags.
+
+![Execution](image/README/1738861298984.png "Remove Host Tags")
+
+## Prerequisites
+
+- Python 3.x
+- Required Python packages:
+  - `argparse`
+  - `colorama`
+  - `requests`
+  - `pandas`
+  - `jwt`
+
+You can install the required packages using the following command:
+
+```sh
+pip install argparse colorama requests pandas pyjwt
+```
+
+## Usage
+
+To run the script, use the following command:
+
+```sh
+python remove_host_tags.py -creds_file <path_to_credentials_file> -csv_file <path_to_csv_file>
+```
+
+### Arguments
+
+- `-creds_file` or `--credentials_file`: Path to the credentials file (required).
+- `-csv_file` or `--csv_file`: Path to the CSV file containing columns `asset_id` and `tag_name` (required).
+
+### Example
+
+```sh
+python remove_host_tags.py -creds_file credentials.json -csv_file tags.csv
+```
+
+## Credential File Format (JSON)
+
+Credential file should contain following values:
+
+- `customerId`: The unique ID of the uptycs tenant
+- `key`: API KEY
+- `secret`: API SECRET
+- `domain`: domain name of uptycs tenant
+- `domainSuffix`: domainSuffix of the uptycs tenant (eg: uptycs.io)
+
+Example JSON File:
+
+```json
+{
+  "id": "90ec4f1a-e764-4f90-b3b8-cbf0ee295fd9",
+  "userId": "751c8192-e99e-442b-ba80-9ee5bc28a9d5",
+  "customerId": "049dad7d-94fc-4d78-9740-f98d81e0a3f1",
+  "active": true,
+  "isInternal": false,
+  "key": "HRYDDCKMJCEI6OLZLBMNBJH7L0XIAFCI",
+  "secret": "pyi60bcqgMKsLsf78Ko0tR9TRON9/qclZJyzg3kK7+dHO4M53b+I88DNSyN/V2Gf",
+  "expiry": null,
+  "updatedAt": "2024-08-28T17:05:24.312Z",
+  "createdAt": "2024-08-28T17:05:24.312Z",
+  "domainSuffix": ".uptycs.io",
+  "domain": "sparrow"
+}
+```
+
+## CSV File Format
+
+The CSV file should contain the following columns:
+
+- `asset_id`: The ID of the asset.
+- `tag_name`: The name of the tag to be removed.
+
+Example CSV file:
+
+```csv
+asset_id,tag_name
+7bd784f7-0d2b-4e48-b79b-9e8674218625,tag1
+1a1557fc-6134-450d-b309-416734b70a80,tag1
+1a1557fc-6134-450d-b309-416734b70a80,tag2
+```
+
+## Script Details
+
+### Functions
+
+- `new_line()`: Prints a new line.
+- `wait_a_second()`: Pauses the script for 1 second.
+- `parse_args()`: Parses command-line arguments.
+- `get_full_path(file_path)`: Returns the full path of the given file.
+- `read_csv_file(file_path)`: Reads the CSV file and returns a dictionary of asset_id and tags to be removed from that asset.
+- `read_credentials(file_path, expiry)`: Reads the credentials file and generates an bearer authorization token.
+- `get_base_url(credentials)`: Constructs the base URL using the credentials.
+- `get_tags(credentials, asset_id)`: Retrieves the tags for the given asset ID.
+- `update_tags(credentials, new_tags, asset_id)`: Updates (removes) the tags for the given asset ID.
+- `showError(data)`: Displays error context if the user opts to see more details.
+- `remove_tags(credentials, host_tag_dict)`: Removes the specified tags from the hosts.
+- `main()`: Main function that orchestrates the script execution.
+
+### Main Execution Flow
+
+1. Parse command-line arguments.
+2. Get the full paths of the credentials and CSV files.
+3. Read the credentials and CSV file.
+4. Retrieve and remove the specified tags from the hosts.
+
+## License
+
+This project is licensed under the MIT License.
---
+++ Remove Tags/remove_host_tags.py
@@ -0,0 +1,210 @@
+try:
+    import os
+    import argparse
+    import pandas as pd
+    import requests
+    import datetime as dt
+    import colorama
+    import jwt
+    import json
+    import time
+except ImportError as e:
+    print("Import Error: please install below modules\n\n pip install pyJWT pandas requests colorama\n\n")
+    exit(1)
+
+colorama.init(autoreset=True)
+
+def new_line():
+    print()
+
+
+def wait_a_second():
+    time.sleep(1)
+    
+def parse_args():
+    try:
+        parser = argparse.ArgumentParser(description="Remove Host Tags")
+        parser.add_argument("-creds_file", "--credentials_file", help="Credentials file path", required=True)
+        parser.add_argument("-csv_file", "--csv_file", help="CSV file path with columns asset_id and tag_name", required=True)
+        args = parser.parse_args()
+        if args.credentials_file is None or args.csv_file is None:
+            print(colorama.Fore.RED + "\n⛔ Please provide all the required arguments")
+            exit(1)
+        return args
+    except Exception as e:
+        print(colorama.Fore.RED + f"\n⛔ Error while parsing arguments: {e}")
+        exit(1)
+
+def get_full_path(file_path):
+    try:
+        if len(file_path.split("/")) == 1:
+            return os.path.join(os.getcwd(), file_path)
+        return file_path
+    except Exception as e:
+        print(colorama.Fore.RED + f"\n⛔ Error while getting full path: {e}")
+        exit(1)
+
+def read_csv_file(file_path):
+    try:
+        df = pd.read_csv(file_path)
+        if "asset_id" not in df.columns or "tag_name" not in df.columns:
+            print(colorama.Fore.RED + f"❌ Columns asset_id and tag_name not found in {file_path} file")
+            exit(1)
+        print(colorama.Fore.MAGENTA + f"✅ {file_path} file read successfully\n")
+        
+        host_tags_dict = {}
+        for _, row in df.iterrows():
+            asset_id = row["asset_id"]
+            tag_name = row["tag_name"]
+            host_tags_dict[asset_id] = host_tags_dict.get(asset_id, []) + [tag_name]
+        return host_tags_dict
+    except FileNotFoundError as e:
+        print(colorama.Fore.RED + f"⛔ {file_path} File Not Found: please check the file path")
+        exit(1)
+    except Exception as e:
+        print(colorama.Fore.RED + "⛔ Error: ", e)
+        exit(1)
+
+
+def read_credentials(file_path, expiry):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.loads(file.read())
+    except FileNotFoundError:
+        print(colorama.Fore.RED + f"\n⛔ {file_path} FILE NOT FOUND, PLEASE PROVIDE CORRECT FILE PATH\n")
+        exit(-1)
+    except json.decoder.JSONDecodeError:
+        print(colorama.Fore.RED + f"\n⛔ PROVIDED FILE {file_path} IS NOT IN JSON FORMAT\n")
+        exit(-1)
+    except Exception as e:
+        print(colorama.Fore.RED + "\n⛔ Error: ", e)
+        exit(1)
+    
+    
+    try:
+        exp = time.time() + expiry
+        expiry = dt.datetime.fromtimestamp(exp).strftime('%Y-%m-%d %H:%M')
+
+        domain = data["domain"] + data["domainSuffix"]
+        key, secret = data["key"], data["secret"]
+        token = jwt.encode({"iss":key, "exp":exp},secret)
+        auth = f"Bearer {token}"
+        credentials = {"domain":domain,"customerId":data["customerId"]}
+        credentials["Expiration"] = expiry
+        credentials["Authorization"] = auth
+        print (colorama.Fore.MAGENTA + f"\n✅ Credentials read for {domain}\n")
+    except Exception as e:
+        print(colorama.Fore.RED + f"\n⛔ Encountered error while generating Auth Token, Error: {e}")
+        exit(-1)
+
+    return credentials
+
+
+def get_base_url(credentials):
+    domain = credentials["domain"]
+    customerId = credentials["customerId"]
+    return f"https://{domain}/public/api/customers/{customerId}"
+
+
+def get_tags(credentials, asset_id) -> list:
+    BASE_URL = get_base_url(credentials)
+    url = f"{BASE_URL}/assets/{asset_id}"
+    
+    try:
+        response = requests.get(url, headers=credentials)
+        data = response.json()
+        return data.get("tags", []), data.get("hostName", "Unknown")
+    except Exception as e:
+        print(colorama.Fore.RED + f"\t⛔ Error while retrieving asset tags for {asset_id}: {e}")
+        exit(1)
+    
+def update_tags(credentials, new_tags, asset_id):
+    BASE_URL = get_base_url(credentials)
+    url = f"{BASE_URL}/assets/{asset_id}"
+    data = {"tags": new_tags}
+    
+    try:
+        response = requests.put(url, headers=credentials, data=data)
+        return response
+    except Exception as e:
+        print(colorama.Fore.RED + f"\t⛔ Error while updating/removing tags from asset {asset_id}\n Error: {e}")
+
+
+def showError(data):
+    opinion = input("\tDo you want to display more context about the error (y/Yes)\n")
+    if opinion.lower() in ["y", "yes", ""]:
+        print(colorama.Fore.YELLOW + data)
+    exit(-1)
+
+
+
+def remove_tags(credentials, host_tag_dict:dict):
+    index = 1
+    for asset_id, tags_to_be_removed in host_tag_dict.items():
+        asset_tags, host_name = get_tags(credentials, asset_id)
+        print(colorama.Fore.LIGHTBLUE_EX + f"\n({index}) Host Name --> {colorama.Fore.YELLOW + f" {host_name} "}", end='')
+        print(colorama.Fore.LIGHTBLUE_EX + f" Asset Id --> {colorama.Fore.YELLOW + f" {asset_id} "}")
+        print(colorama.Fore.LIGHTGREEN_EX + f"\n\t→ Found {len(asset_tags)} tags in the Asset")
+        print(colorama.Fore.LIGHTCYAN_EX + f"\n\t→ Tags to be removed: {colorama.Fore.LIGHTWHITE_EX + f" {tags_to_be_removed} "}")
+
+        missing_tags = [tag for tag in tags_to_be_removed if tag not in asset_tags]
+        
+        if len(missing_tags) == len(tags_to_be_removed):
+            print(colorama.Fore.LIGHTRED_EX + f"\n\t→ ❌ No provided tags found in the asset")
+            index += 1
+            continue
+        for tag in missing_tags:
+            print(colorama.Fore.LIGHTRED_EX + f"\n\t→ ❌ Tag {tag} not found in the Asset")
+            tags_to_be_removed.remove(tag)
+                        
+        print(colorama.Fore.LIGHTYELLOW_EX + f'\n\t→ Removing "{len(tags_to_be_removed)}" tag{'s' if len(tags_to_be_removed) > 1 else ''} from Asset "{host_name}"')
+        
+        new_asset_tags = [tag for tag in asset_tags if tag not in tags_to_be_removed]
+        
+        response = update_tags(credentials, new_asset_tags, asset_id)
+        status = response.status_code
+        print(colorama.Fore.LIGHTYELLOW_EX + f"\n\t→ Response Status : {status}\n")
+        if response.status_code != 200:
+            showError(json.dumps(response.json()))
+        print(colorama.Fore.LIGHTGREEN_EX + f'\n\t→ ✅ Tags {tags_to_be_removed} removed from the {asset_id}')
+        index += 1
+    new_line()
+    new_line()
+    print(colorama.Back.WHITE + colorama.Fore.GREEN + " ✅ All Tags Removed Successfully ")
+    new_line()
+
+
+
+def main():
+    expiry = 3600 # jwt token lifespan
+    print(colorama.Fore.LIGHTBLUE_EX + "\n\n🔥🔥🔥 Welcome to Remove Host Tags Script 🔥🔥🔥\n")
+    
+    wait_a_second()
+        
+    args = parse_args()
+    credentials_path = args.credentials_file
+    csv_path = args.csv_file
+    print(colorama.Fore.MAGENTA + f"\n✅ Arguments Parsed Successfully")
+    wait_a_second()
+    
+    print(colorama.Fore.CYAN + f"\nRecieved Following Paths: \n")
+    print(colorama.Fore.LIGHTYELLOW_EX + f"\tCredentials File: {colorama.Fore.LIGHTWHITE_EX + f'{credentials_path}'}")
+    print(colorama.Fore.LIGHTYELLOW_EX + f"\tCSV File: {colorama.Fore.LIGHTWHITE_EX + f'{csv_path}'}")
+    wait_a_second()
+    
+    credentials_path = get_full_path(credentials_path)
+    csv_path = get_full_path(csv_path)
+
+    credentials = read_credentials(credentials_path, expiry)
+    wait_a_second()
+    host_tag_dict = read_csv_file(csv_path)
+    wait_a_second()
+    
+    print(colorama.Fore.CYAN + f"\n\t\t\t\t\t⌛ R̲e̲m̲o̲v̲i̲n̲g̲ T̲a̲g̲s̲ ⌛")
+    remove_tags(credentials, host_tag_dict)
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
